3954c8ce7b679bce1b40596e60decce344dc680e
aveendazy <zaveenda@mail.ugm.ac.id>
2022-06-22 01:53:34 +0700

    fitur Localization (language)

--- app/src/main/java/com/example/scheduleapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/scheduleapp/Activity/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.scheduleapp.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -19,11 +26,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.days_card);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         cardMonday = (CardView) findViewById(R.id.card_monday);
         cardTuesday = (CardView) findViewById(R.id.card_tuesday);
 
         cardMonday.setOnClickListener(this);
         cardTuesday.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.language){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
